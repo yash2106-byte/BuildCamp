ba0bd06b790a85cbaa42488d4b6b5912e23c194a
yash2106-byte <rajyash2510@gmail.com>
2026-03-30 20:42:09 +0530

    CORS CONFIGURATIONS COMPLETED

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,21 @@
 import express from 'express'
-
+import cors from 'cors'
 const web = express()
+
+// these are the bare minimum configurations
+web.use(express.json({limit: "16kb"}))
+web.use(express.urlencoded({extended: true,limits:"16kb"}))
+web.use(express.static('public'))
+
+// cors configurations
+web.use(cors({
+    origin: process.env.CORSORIGIN?.split(",")|| "http://localhost:5172",
+    credentials:true,
+    methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+    allowedHeaders:["Authorization","Content-Type"],
+    }),
+)
+
 
 web.get('/',(req,res)=>{
     res.send("this is the home page")
