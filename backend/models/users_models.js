936d0719ced82f165df9936ea67fc22aeab94405
yash2106-byte <rajyash2510@gmail.com>
2026-04-07 17:37:47 +0530

    setup auth proces

--- backend/models/users_models.js
+++ backend/models/users_models.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose"
 import brcypt from "brcypt"
+import jwt from "jsonwebtoken"
+import cryto from "crypto"
 
 const userSchema = new Schema({
     avatar:{
@@ -70,7 +72,36 @@ userSchema.method.isPasswordCorrect = async function (password) {
     return await brcypt.compare(password,this.password)    
 };
 
+// we are generating Access tokens below ,for every user request this function we be called
+userSchema.method.GenerateAcces = function(){
+    // this will be the payload which will be returned 
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+// THIS IS GENREATING THE REFRESH TOKEN
+userSchema.method.GenerateRefresh = function(){
+    // this will be the payload which will be returned 
+    return jwt.sign({
+        _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+// genrating a hashed string  
+userSchema.method.GenerateTemp = function(){
+    const unhashed = cryto.randomBytes(20).toString("hex")
+    const hashed = cryto.createHash("sha256").update(unhashed).digest("hex")
+    const tokenExpiry = Date.now() + (20*60*100)
+    return {unhashed,hashed,tokenExpiry}
+}
 
 export const User = mongoose.model("User",userSchema)
 
 
+ 
