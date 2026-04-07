c81c570aeed80899a601c719647df0f860b8abf6
yash2106-byte <rajyash2510@gmail.com>
2026-04-07 18:45:28 +0530

    prepared a fully functioning sending mail system

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -1,6 +1,49 @@
+// This is the file where we will design the body of our email which will be send to users when they will login for the first time or when they will reset the password
+// this is just a template , for actually sending these mails we'll use "AWS SES" or "Brevo" but if we are just testing thenwe should prefer "mailtrap"
+
 import Mailgen from "mailgen";
 
+// THIS IS HOW WE WILL SEND THE EMAIL WHICH WE HAVE GENRATED BELOW
+import nodemailer from "nodemailer"
 
+const sendEmail = async (options)=>{
+    // this part has nothing to do with sending the mail it is just branding 
+    const mailGenrator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Task Manager",
+            link: ""
+        }
+    })
+    
+    const emailTextual = mailGenrator.generatePlaintext(options.mailgenContent)// this is for thoese people whose mail doent support html file
+    const emailHTML = mailGenrator.generate(options.mailgenContent)// this is the mail in the html format
+
+    const transpoter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port:process.env.MAILTRAP_SMTP_PORT,
+        auth:{
+            user: process.env.MAILTRAP_SMTP_USER,
+            pass: process.env.MAILTRAP_SMTP_PASS
+        }
+    })
+
+    const mail = {
+        from: "mail.sendermail@BaseCamp.com",
+        to: options.email,
+        subject: options.subject,
+        text: options.emailTextual,
+        html: options.emailHTML
+    }
+
+    try {
+        await transpoter.sendMail(mail)
+    } catch (error) {
+        console.error("Email Service has failed silently because of an invalid credit")
+        console.error("Error:" ,error)
+        
+    }
+}
 
 const emailVerficationMailgenContent = (username,verficationUrl)=>{
     return {
@@ -18,7 +61,7 @@ const emailVerficationMailgenContent = (username,verficationUrl)=>{
 
     }
 }
-
+ 
 const FogetPasswordMailgenContent = (username,passwordRestUrl)=>{
     return {
         body:username,
@@ -38,5 +81,6 @@ const FogetPasswordMailgenContent = (username,passwordRestUrl)=>{
 
 export{
     emailVerficationMailgenContent,
-    FogetPasswordMailgenContent
+    FogetPasswordMailgenContent,
+    sendEmail
 }
