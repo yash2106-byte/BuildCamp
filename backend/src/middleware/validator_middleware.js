a07a29fa6b3782c37b0dbdfcd0829f12aebf3fa7
yash2106-byte <rajyash2510@gmail.com>
2026-04-29 18:58:56 +0530

    set up a validator system

---
+++ backend/src/middleware/validator_middleware.js
@@ -0,0 +1,19 @@
+// this helps you to validate user input this works as a middleware
+import { validationResult } from 'express-validator'
+import { ApiError } from "../utils/api-error.js"
+
+
+// we'll be using this validationResult in the other part but for now we are just giveing it a req which will be checked by this function and is there is an Error then it will be given out using our error functions
+export const validate = (req,res,next)=>{
+    const error = validationResult(req)
+    if(error.isEmpty()){
+        return next()
+    }
+    // if there are some errors then we are converting it into an array so that we can loop through it once we have made the array then we'll push with error path and error message in a pair
+    const extractedError = []
+    error.array().mpp((err)=> extractedError.push(
+        {
+            [err.path]:err.msg
+        }));
+        throw new ApiError(422,"Recived data is not valid",extractedError)
+}
---
+++ backend/src/validators/index.js
@@ -0,0 +1,28 @@
+import { body } from "express-validator"
+
+// over here we'll take each parameter coming from the frontend and then eventually check them using inbuilt functions
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email not found")
+            .isEmail()
+            .withMessage("Email is not valid"),
+
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is required")
+            .isLowercase()
+            .withMessage("Username must be in lower case")
+            .isLength({min: 6})
+            .withMessage("Username should of atleast 6 characters"),
+        
+        body("password")
+            .trim()
+            .notEmpty()
+            .isLength({min: 6})
+            .withMessage("Password should be more than 6 characters"),
+    ]
+}
