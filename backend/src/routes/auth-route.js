b557eb29f59932b7d84228495ee91439eca3c0ba
yash2106-byte <rajyash2510@gmail.com>
2026-05-01 19:28:24 +0530

    added validator for the login function

--- backend/src/routes/auth-route.js
+++ backend/src/routes/auth-route.js
@@ -1,13 +1,13 @@
 import { Router } from "express"
 import { RegisterUser,loginfunction } from "../controllers/auth.controllers.js"
 import { validate } from "../middleware/validator_middleware.js"
-import { userRegisterValidator } from "../validators/index.js"
+import { userRegisterValidator,userLoginValidator } from "../validators/index.js"
 
 const router = Router()
 
 // middleware sections
 router.route("/register").post(userRegisterValidator(),validate,RegisterUser)
-router.route("/login").post(loginfunction)
+router.route("/login").post(userLoginValidator(),validate,loginfunction)
 
 
 export default router
--- backend/src/validators/index.js
+++ backend/src/validators/index.js
@@ -1,7 +1,7 @@
 import { body } from "express-validator"
 
 // over here we'll take each parameter coming from the frontend and then eventually check them using inbuilt functions
-export const userRegisterValidator = () => {
+const userRegisterValidator = () => {
     return [
         body("email")
             .trim()
@@ -26,3 +26,21 @@ export const userRegisterValidator = () => {
             .withMessage("Password should be more than 6 characters"),
     ]
 }
+
+const userLoginValidator = () => {
+    return [
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email not found")
+            .isEmail()
+            .withMessage("Email is not valid"),
+        
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required")
+    ]
+}
+
+export {userRegisterValidator,userLoginValidator}
